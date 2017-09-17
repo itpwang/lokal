5196302ade9e76d88977e399c774de43eee878d4
Ivan Wang <ivan.wang.1995@gmail.com>
2017-09-17 05:27:58 -0700

    Update index.js to save data from /post route.

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
+var util = require('util');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-   title: 'Express'
+  res.render('event_setup_form1', {
    });
 });
-	
+	 
 router.get('/post', function(req, res){
 	// res.render('event_setup_form1')
-	res.render('event_form')
+	res.render('event_form');
 });
 
-// router.post('/post', function(req,res){ res.post()})
+router.post('/post', function(req,res){
+ 	var form = new formidable.IncomingForm();
+
+ 	form.parse(req, function(err,fields, files){
+ 		res.writeHead(200, { 'content-type': 'text/plain'});
+ 		res.write('received the data:\n\n');
+ 		
+ 		
+ 		if(!fs.existsSync('events.json')){
+ 			var data = {
+ 			table : []
+ 			}
+ 			data.table.push({fields});
+ 			var json = JSON.stringify(data);
+ 			fs.writeFile('events.json', json, 'utf8', (err)=>{
+ 				if(err) throw err;
+ 				console.log('The file has been created & saved.')
+ 			});
+ 		}
+ 		else{
+ 			fs.readFile('events.json', 'utf8' , function readFileCallback(err, data){
+ 				if(err){
+ 					console.log(err);
+ 				}
+ 				else {
+ 					data = JSON.parse(data);
+ 					data.table.push(fields);
+ 					json = JSON.stringify(data);
+ 					fs.writeFile('events.json' , json, 'utf8', (err)=>{
+ 					if(err) throw err;
+ 					console.log('The file has been appended & saved.');
+ 					});
+ 				}
+ 			})
+ 		}
+ 		res.end(util.inspect({
+ 			fields: fields,
+ 			files: files
+ 			}));
+ 		});
+ 	});
+ 
+
 module.exports = router;
